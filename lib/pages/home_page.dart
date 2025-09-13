6dba50139fa37d6ca23ec87949fb95cccab57555
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-09-12 20:13:27 -0500

    agregando positioned a los elementos de un stack

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -98,8 +98,10 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Container(
                           padding: EdgeInsets.symmetric(
@@ -115,11 +117,83 @@ class HomePage extends StatelessWidget {
                             style: TextStyle(color: Colors.white),
                           ),
                         ),
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Positioned(
+                              right: 40,
+                              child: CircleAvatar(
+                                radius: 16,
+                                backgroundImage: NetworkImage(
+                                  "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?_gl=1*1xbsusj*_ga*ODI4MzUxMDczLjE3MjI2NDc0MzI.*_ga_8JE65Q40S6*czE3NTc3MjQ0NjIkbzcwJGcxJHQxNzU3NzI1NjA1JGozMSRsMCRoMA..",
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              right: 20,
+                              child: CircleAvatar(
+                                radius: 16,
+                                backgroundImage: NetworkImage(
+                                  "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?_gl=1*1e3a13f*_ga*ODI4MzUxMDczLjE3MjI2NDc0MzI.*_ga_8JE65Q40S6*czE3NTc3MjQ0NjIkbzcwJGcxJHQxNzU3NzI1NTg3JGo0OSRsMCRoMA..",
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              child: CircleAvatar(
+                                radius: 16,
+                                backgroundImage: NetworkImage(
+                                  "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?_gl=1*cmhs3e*_ga*ODI4MzUxMDczLjE3MjI2NDc0MzI.*_ga_8JE65Q40S6*czE3NTc3MjQ0NjIkbzcwJGcxJHQxNzU3NzI1NjEyJGoyNCRsMCRoMA..",
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text("Thailand", style: TextStyle(color: Colors.white)),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Thailand",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              "18 tours",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 4,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.35),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                "4.5",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Icon(Icons.star, color: Colors.white),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ],
